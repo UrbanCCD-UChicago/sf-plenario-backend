1857f68472676a9e906204f45cc25dfb942bb122
apanella <breadshe@gmail.com>
2014-07-29 11:41:46 -0500

    Added submodule for Voronoi triangulation

--- plenario_sf.py
+++ plenario_sf.py
@@ -6,6 +6,5 @@ app = create_app()
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
-    #app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
     #app.run(debug=True, use_reloader=False)
     app.run()
